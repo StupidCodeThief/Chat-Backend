8e2a8e4221fa5ddb8b73e56531addd8bab5d228f
Denis Vasiliuk <53581175+StupidCodeThief@users.noreply.github.com>
2020-11-23 13:01:50 +0300

    Update userAlreadyExistsError.js

--- api/services/errorHandlers/userAlreadyExistsError.js
+++ api/services/errorHandlers/userAlreadyExistsError.js
@@ -2,7 +2,7 @@ const ApplicationError = require("./applicationError");
 
 class UserAlreadyExists extends ApplicationError {
   constructor(message) {
-    super(message || 'User already exists.', 403);
+    super(message || 'User already exists.', 404);
   }
 }
 
